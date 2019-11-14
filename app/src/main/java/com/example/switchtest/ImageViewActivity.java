db429b66e32dd47772a6fb550457ac9896252f86
YuJie <mmmmhhhhhhhhhhhh@gmail.com>
2019-11-14 15:22:29 +0800

    10/18　開始學切換Activity(跳頁)

--- app/src/main/java/com/example/switchtest/ImageViewActivity.java
+++ app/src/main/java/com/example/switchtest/ImageViewActivity.java
@@ -9,27 +9,29 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class ImageViewActivity extends AppCompatActivity {
-    TextView txt_msg;
-    Intent intent;
-    Bundle get_bag;
+//9(13行)
+      TextView txt_msg;
+//7
+      Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
-        Button btn_back = findViewById(R.id.buttonBack);
-        txt_msg = findViewById(R.id.txtmsg);
-        intent = new Intent();
 
-        get_bag = getIntent().getExtras();
-        String num = get_bag.getString("NUM");
-        int salary = get_bag.getInt("SALARY");
-        String name = get_bag.getString("NAME");
-        String phone = get_bag.getString("PHONE");
-        txt_msg.setText("姓名"+name+"\n電話"+phone+"\n"+num+salary);
+        Button btn_back = findViewById(R.id.buttonBack);
+//10(25-29)
+        txt_msg = findViewById(R.id.txtmsg);
+        intent = getIntent();
+        String num = intent.getStringExtra("MSG");
+        int salary = intent.getIntExtra("SALARY",0);
+        txt_msg.setText(num+salary);
+
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 intent = new Intent();
                 intent.setClass(ImageViewActivity.this,MainActivity.class);
                 startActivity(intent);
--- app/src/main/java/com/example/switchtest/MainActivity.java
+++ app/src/main/java/com/example/switchtest/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.switchtest;
-
+//開始學習切換Activity(顯性的意圖)
+//1主介面拉TextView跟Button
+//2新增一個Empty Activity(ImageView),都會附一個同名layout
+//3在mipmap給一張圖,在介面拉ImageView,在自動資源視窗選Project定位
+//4在介面拉Button,定位
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,24 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//5
         Button btn_next = findViewById(R.id.buttonNext);
 
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                Bundle bag = new Bundle();
-                bag.putString("MSG","本月薪資");
-                bag.putInt("SALARY",50000);
-                bag.putString("NAME","王小明");
-                bag.putString("PHONE","0988444555");
-
-                intent.putExtras(bag);
+//8(28-30行)區別在傳值過去(簡單的如果只有兩個值要傳用putExtra,多些就要用bundle了)
+                intent.putExtra("MSG","本月薪資:");
+                       //傳值過去
+                intent.putExtra("SALARY",50000);
+//6
                 intent.setClass(MainActivity.this,ImageViewActivity.class);
-
+                                    //Context是現在的activity,     class是要跳過去的activity　
                 startActivity(intent);
-                finish();
+                finish();//讓現在activity結束(暫停)
             }
         });
     }
